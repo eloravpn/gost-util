d3d8808937e4b31a31ef199fa9e762f4ad7b01cc
Nader Jafari <n.jafari@sain.ir>
2023-12-30 16:26:57 +0330

    Add port range in csv

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import ast
 import csv
+import json
 
 import yaml
 
@@ -9,7 +11,7 @@ if __name__ == "__main__":
 
     user_name = "gost"
     password = "gost"
-    connection_limit = 100
+    connection_limit = 1000
     limit = "2MB"
     request_limit = "5"
 
@@ -24,33 +26,44 @@ if __name__ == "__main__":
     rlimiters = []
 
     for index, row in enumerate(csvreader):
-        password = row[3]
+        in_port = row[0]
+        ip = row[1]
+
+        start_port = int(row[2])
+        end_port = int(row[3])
+        step = int(row[4])
+
+        password = row[5]
+
+        nodes = []
+
         services.append(
             {
-                "name": f"service-{index+1}",
+                "name": f"service-{in_port}",
                 "climiter": "climiter-0",
-                "limiter": "limiter-0",
-                "rlimiter": "rlimiter-0",
-                "addr": f":{row[0]}",
-                "handler": Handler(chain=f"chain-{index+1}", type_="tcp"),
+                # "limiter": "limiter-0",
+                # "rlimiter": "rlimiter-0",
+                "addr": f":{in_port}",
+                "handler": Handler(chain=f"chain-{in_port}", type_="tcp"),
                 "listener": Listener(type_="tcp"),
             }
         )
 
-        nodes = [
-            Node(
-                name=f"node-1",
-                addr=f"{row[1]}:{row[2]}",
-                connector=Connector(type_="sshd"),
-                dialer=Dialer(
-                    auth=Auth(username=user_name, password=password), type_="sshd"
-                ),
+        for i in range(start_port, end_port + 1, step):
+            nodes.append(
+                Node(
+                    name=f"node-{i}",
+                    addr=f"{ip}:{i}",
+                    connector=Connector(type_="sshd"),
+                    dialer=Dialer(
+                        auth=Auth(username=user_name, password=password), type_="sshd"
+                    ),
+                )
             )
-        ]
 
         chains.append(
             {
-                "name": f"chain-{index+1}",
+                "name": f"chain-{in_port}",
                 "hops": [{"name": f"hope-1", "nodes": nodes}],
             }
         )
@@ -62,26 +75,26 @@ if __name__ == "__main__":
         }
     )
 
-    limiters.append(
-        {
-            "name": "limiter-0",
-            "limits": [f"$$ {limit}"],
-        }
-    )
-
-    rlimiters.append(
-        {
-            "name": "rlimiter-0",
-            "limits": [f"$$ {request_limit}"],
-        }
-    )
+    # limiters.append(
+    #     {
+    #         "name": "limiter-0",
+    #         "limits": [f"$$ {limit}"],
+    #     }
+    # )
+    #
+    # rlimiters.append(
+    #     {
+    #         "name": "rlimiter-0",
+    #         "limits": [f"$$ {request_limit}"],
+    #     }
+    # )
 
     gost = {
         "services": services,
         "chains": chains,
         "climiters": climiters,
-        "limiters": limiters,
-        "rlimiters": rlimiters,
+        # "limiters": limiters,
+        # "rlimiters": rlimiters,
     }
 
     with open("gost.yaml", "w", encoding="utf-8") as f:
